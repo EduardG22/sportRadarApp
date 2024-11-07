94eb9231f06d30bc441cbf1511345acf122931d8
EduardG22 <popaeduard22@gmail.com>
2024-11-07 20:37:55 +0100

    set up the project

---
+++ src/main/java/eduardpopa/sportRadarApp/SportRadarAppApplication.java
@@ -0,0 +1,13 @@
+package eduardpopa.sportRadarApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportRadarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportRadarAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eduardpopa/sportRadarApp/SportRadarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package eduardpopa.sportRadarApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportRadarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
